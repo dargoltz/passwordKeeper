3e4aabac59f2944fec22c8d1f494368a222dd0a7
darg0ltz <xfoj@bk.ru>
2024-02-18 16:20:37 +0300

    feat: make request proccesing async

--- app/controllers/MainController.scala
+++ app/controllers/MainController.scala
@@ -16,8 +16,10 @@ class MainController @Inject()(
                                 val notesUpdateLogService: NoteUpdateLogService
                               )(implicit ec: ExecutionContext) extends BaseController {
 
-  def getNotes(name: String): Action[AnyContent] = Action {
-    Ok(Json.toJson(notesService.getNotes(name)))
+  def getNotes(name: String): Action[AnyContent] = Action.async { _ =>
+    notesService.getNotes(name).map { historyLog =>
+      Ok(Json.toJson(historyLog))
+    }
   }
 
   def createNote: Action[JsValue] = Action(parse.json) { request =>
@@ -26,29 +28,35 @@ class MainController @Inject()(
       errors => {
         BadRequest(Json.obj("status" -> "error", "message" -> JsError.toJson(errors)))
       },
-      noteDTO => {
-        notesService.createNote(noteDTO)
-        Ok("Note for " + noteDTO.name + " created")
+      newNote => {
+        notesService.createNote(newNote)
+        Ok("Note for " + newNote.name + " created")
       }
     )
   }
 
-  def updateNote(id: Int, password: String): Action[AnyContent] = Action {
-    notesService.updateNotePassword(id, password)
-    Ok("Password for note " + id + " updated")
+  def updateNote(id: Int, password: String): Action[AnyContent] = Action.async { _ =>
+    notesService.updateNotePassword(id, password).map { _ =>
+      Ok("Password for note " + id + " updated")
+    }
   }
 
-  def deleteNote(id: Int): Action[AnyContent] = Action {
-    notesService.deleteNote(id)
-    Ok("Note " + id + " deleted")
+  def deleteNote(id: Int): Action[AnyContent] = Action.async { _ =>
+    notesService.deleteNote(id).map { _ =>
+      Ok("Note " + id + " deleted")
+    }
   }
 
-  def getHistoryLogAll: Action[AnyContent] = Action {
-    Ok(Json.toJson(notesUpdateLogService.getHistoryLog()))
+  def getHistoryLogAll: Action[AnyContent] = Action.async { _ =>
+    notesUpdateLogService.getHistoryLog().map { historyLog =>
+      Ok(Json.toJson(historyLog))
+    }
   }
 
-  def getHistoryLogByNoteId(noteId: Int): Action[AnyContent] = Action {
-    Ok(Json.toJson(notesUpdateLogService.getHistoryLog(Some(noteId))))
+  def getHistoryLogByNoteId(noteId: Int): Action[AnyContent] = Action.async { _ =>
+    notesUpdateLogService.getHistoryLog(Some(noteId)).map { historyLog =>
+      Ok(Json.toJson(historyLog))
+    }
   }
 
   def importNotes: Action[MultipartFormData[Files.TemporaryFile]] = Action.async(parse.multipartFormData) { request =>
@@ -58,22 +66,24 @@ class MainController @Inject()(
     if (csvFiles.isEmpty) {
       Future.successful(BadRequest("Файл CSV не найден"))
     }
-    val importFutures = csvFiles.map { csvFile =>
+    val importResult = csvFiles.map { csvFile =>
       notesService.importFromCSV(csvFile.ref)
     }
-    Future.sequence(importFutures).map { _ =>
+    Future.sequence(importResult).map { _ =>
       Ok("Импорт завершен успешно")
     }
   }
 
-  def exportNotes: Action[AnyContent] = Action { _ =>
-    val tempFile = notesService.exportToSCV()
-    Ok.sendFile(
-      content = tempFile,
-    ).withHeaders(
-      CONTENT_TYPE -> "text/csv",
-      CONTENT_DISPOSITION -> s"inline; filename=tempFile.csv"
-    )
+  def exportNotes: Action[AnyContent] = Action.async { _ =>
+    val futureTempFile = notesService.exportToSCV()
+
+    futureTempFile.map { tempFile =>
+      Ok.sendFile(
+        content = tempFile,
+      ).withHeaders(
+        CONTENT_TYPE -> "text/csv",
+      )
+    }
   }
 
 
