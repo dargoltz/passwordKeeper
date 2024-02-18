dfbd7a8f3052eaec4259ad5dc7dc0cda698b41d9
darg0ltz <xfoj@bk.ru>
2024-02-18 16:21:55 +0300

    feat: add timestamp formatter

--- app/model/NoteUpdateLog.scala
+++ app/model/NoteUpdateLog.scala
@@ -1,6 +1,6 @@
 package model
 
-import play.api.libs.json.{Format, Json}
+import play.api.libs.json.{Format, JsResult, JsValue, Json}
 
 import java.sql.Timestamp
 
@@ -8,4 +8,9 @@ case class NoteUpdateLog(id: Int, noteId: Int, action: String, changed: Timestam
 
 object NoteUpdateLog {
   implicit val format: Format[NoteUpdateLog] = Json.format[NoteUpdateLog]
+
+  implicit val timestampFormat: Format[Timestamp] = new Format[Timestamp] {
+    def writes(t: Timestamp): JsValue = Json.toJson(t.getTime)
+    def reads(json: JsValue): JsResult[Timestamp] = json.validate[Long].map(new Timestamp(_))
+  }
 }
