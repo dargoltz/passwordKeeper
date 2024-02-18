a0e4352c234360331f4bb7e094036586b3df24b9
darg0ltz <xfoj@bk.ru>
2024-02-18 14:00:42 +0300

    feat: change datetime type to timestamp

--- app/model/NoteUpdateLog.scala
+++ app/model/NoteUpdateLog.scala
@@ -2,9 +2,9 @@ package model
 
 import play.api.libs.json.{Format, Json}
 
-import java.time.LocalDateTime
+import java.sql.Timestamp
 
-case class NoteUpdateLog(id: Option[Int], noteId: Int, action: String, changed: LocalDateTime, newPassword: Option[String], oldPassword: Option[String])
+case class NoteUpdateLog(id: Option[Int], noteId: Int, action: String, changed: Timestamp, newPassword: Option[String], oldPassword: Option[String])
 
 object NoteUpdateLog {
   implicit val format: Format[NoteUpdateLog] = Json.format[NoteUpdateLog]
